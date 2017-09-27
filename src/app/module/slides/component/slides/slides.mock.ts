e3416d552962e34ff20e331e574c5b6399b58647
Martin Lonský <martin.lonsky@appin.cz>
2017-09-27 14:01:17 +0200

    prepare gdd

--- src/app/module/slides/component/slides/slides.mock.ts
+++ src/app/module/slides/component/slides/slides.mock.ts
@@ -30,7 +30,10 @@ export const slides: ISlide[] = [
   {
     name: "KeyNote - Slide Five",
     active: false,
-    img: "gdd5.png",
+    innerHtml: `<iframe width="100%" height="100%" class="iframe"
+src="https://drive.google.com/file/d/0B38-GodXnjxjR0pMRERYcFMyY0k/preview"
+frameborder="0" allowfullscreen></iframe>
+`,
   },
   {
     name: "KeyNote - Slide Six",
@@ -126,7 +129,7 @@ export const slides: ISlide[] = [
     name: "PWA - Slide One",
     active: false,
     innerHtml: `<iframe width="100%" height="100%" class="iframe"
-src="https://www.youtube.com/embed/eodArdGRIVQ?rel=0&amp;showinfo=0&amp;start=1"
+src="https://www.youtube.com/embed/eodArdGRIVQ?rel=0&amp;showinfo=0&amp;start=1&amp;autoplay=1"
 frameborder="0" allowfullscreen></iframe>
 `,
   },
@@ -134,7 +137,7 @@ frameborder="0" allowfullscreen></iframe>
     name: "Closing - Slide One",
     active: false,
     innerHtml: `<iframe width="100%" height="100%" class="iframe"
-src="https://www.youtube.com/embed/ZgaQn9coYfU?rel=0&amp;showinfo=0&amp;start=1"
+src="https://www.youtube.com/embed/ZgaQn9coYfU?rel=0&amp;showinfo=0&amp;start=1&amp;autoplay=1"
 frameborder="0" allowfullscreen></iframe>
 `,
   },
